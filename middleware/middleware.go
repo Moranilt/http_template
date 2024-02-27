371c65e1e155c4a4b056cef4c3627b4800e86bc5
Daniel Breg <unicornxoxo2@gmail.com>
2024-02-27 11:17:37 +0300

    feat: add otel middleware
    - replace HandlerFunc with Handler

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,6 +8,8 @@ import (
 	"github.com/Moranilt/http-utils/logger"
 	"github.com/Moranilt/http-utils/response"
 	"github.com/google/uuid"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 type ContextKey string
@@ -20,7 +22,7 @@ type Middleware struct {
 	logger logger.Logger
 }
 
-type EndpointMiddlewareFunc func(handleFunc http.HandlerFunc) http.HandlerFunc
+type EndpointMiddlewareFunc func(handleFunc http.Handler) http.Handler
 
 func New(l logger.Logger) *Middleware {
 	return &Middleware{
@@ -40,12 +42,47 @@ func (m *Middleware) Default(next http.Handler) http.Handler {
 	})
 }
 
-func (m *Middleware) AppTokenRequired(handleFunc http.HandlerFunc) http.HandlerFunc {
+func (m *Middleware) Otel(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		newCtx, span := otel.Tracer("http").Start(ctx, r.URL.Path)
+		defer span.End()
+
+		span.SetAttributes(
+			attribute.String("http.method", r.Method),
+			attribute.String("http.route", r.URL.Path),
+			attribute.String("span.kind", "server"),
+			attribute.String("request_id", GetRequestID(r.Context())),
+		)
+
+		rw := &responseWriter{ResponseWriter: w}
+		r = r.WithContext(newCtx)
+		next.ServeHTTP(rw, r)
+
+		span.SetAttributes(attribute.Int("http.status_code", rw.statusCode))
+	})
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func (m *Middleware) AppTokenRequired(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get(TOKEN_HEADER) == "" {
 			response.ErrorResponse(w, fmt.Errorf("%s required", TOKEN_HEADER), http.StatusBadRequest)
 			return
 		}
-		handleFunc(w, r)
+		next.ServeHTTP(w, r)
 	})
+}
+
+func GetRequestID(ctx context.Context) string {
+	return ctx.Value(logger.CtxRequestId).(string)
 }
